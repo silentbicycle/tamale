cd51b609e51e78df2275ff3deffb63e2b8e38bd8
Scott Vokes <vokes.s@gmail.com>
2010-03-06 08:52:30 -0500

    Initial import.

---
+++ tamale.lua
@@ -0,0 +1,105 @@
+--[[
+Copyright (c) 2010 Scott Vokes <vokes.s@gmail.com>
+ 
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+ 
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+ 
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+--]]
+
+
+-- Depenedencies
+local assert, getmetatable, ipairs, pairs, pcall, setmetatable, type =
+   assert, getmetatable, ipairs, pairs, pcall, setmetatable, type
+
+
+---TAble-MAtching Lua Extension.
+module("tamale")
+
+
+local VAR = {}
+local function is_var(t) return getmetatable(t) == VAR end
+local function is_func(f) return type(f) == "function" end
+local function ignore(key) return key:sub(1, 1) == "_" end
+
+
+---Mark a string in a match pattern as a variable key.
+-- Any variables beginning with _ are ignored.
+-- @usage { "extract", {var"_", var"_", var"third", var"_" } }
+function var(name)
+   assert(type(name) == "string", "Variable must be string")
+   return setmetatable( { k=name }, VAR)
+end
+
+
+---Default hook for match failure. Returns (false, "match failed", p).
+function match_fail(p)
+   return false, "Match failed", p
+end
+
+
+-- Try to match val against a pattern, setting variables in the
+-- pattern to the corresponding values in val.
+local function unify(pat, val, env)
+   local pt = type(pat)
+   if pt == "table" then
+      if is_var(pat) then
+         local cur = env[pat.k]
+         if cur and cur ~= val and not ignore(pat.k) then return false end
+         env[pat.k] = val
+         return env
+      end
+      if type(val) ~= "table" or #pat ~= #val then return false end
+      for k,v in pairs(pat) do
+         local u = unify(v, val[k], env)
+         if not u then return false end
+      end
+      return env
+   else                         --just compare as literals
+      return pat == val and env or false
+   end
+end
+
+
+local function do_res(res, u)
+   if is_func(res) then return res(u) else return res, u end
+end
+
+
+---Return a matcher function for a given specification.
+function matcher(spec)
+   return
+   function (t)
+      for i,row in ipairs(spec) do
+         local pat, res, where = row[1], row[2], row.where
+         local u = unify(pat, t, {})
+         if u then
+            u[1] = t         --whole matched value
+            if where then
+               local ok, val = pcall(where, u)
+               if ok and val then return do_res(res, u) end
+            else
+               return do_res(res, u)
+            end
+         end
+      end
+      local fail = spec.default or match_fail
+      return fail(t)
+   end         
+end
---
+++ test.lua
@@ -0,0 +1,114 @@
+require "tamale"
+require "lunatest"
+
+local V = tamale.var
+
+local M
+
+function setup(x)
+   M = tamale.matcher {
+      { 27, "twenty-seven" },
+      { "str", "string" },
+      { { 1, 2, 3},
+        function(t) return "one two three" end },
+      { { 1, {2, "three"}, 4}, function(t) return "success" end },
+      { { "gt3", V"X"}, function(t) return 10 * t.X end,
+        where=function (t) return t.X > 3 end },
+      { { V"a", V"b", V"c", V"b" }, function(t) return "ABCB" end },
+      { { "a", {"b", V"X" }, "c", V"X"},
+        function(t) return "X is " .. t.X end },
+      { { "extract", { V"_", V"_", V"third", V"_" } },
+        function(t) return t.third end },
+   }
+end
+
+
+function test_m()
+   assert_true(M)
+end
+
+function test_literal_num()
+   assert_equal("twenty-seven", M(27))
+end
+
+function test_literal_str()
+   assert_equal("string", M "str")
+end
+
+function test_literal_num_trio()
+   assert_equal("one two three", M {1, 2, 3})
+end
+
+function test_literal_num_trio_too_many()
+   assert_false(M {1, 2, 3, 4})
+end
+
+function test_nomatch()
+   assert_false(M {1, 2, 4})
+end
+
+function test_matchvar()
+   assert_equal(70, M {"gt3", 7})
+end
+
+function test_matchvar_fail()
+   assert_false(M {"gt3", "boo"})
+end
+
+function test_matchvar_nested()
+   assert_equal("success", (M {1, {2, "three"}, 4}))
+end
+
+function test_match_repeated_num_var()
+   assert_equal("ABCB", M {1, 2, 3, 2})
+end
+
+function test_match_repeated_str_var()
+   assert_equal("ABCB", M {"apple", "banana", "corn", "banana"})
+end
+
+function test_match_repeated_table_var()
+   local apple, banana, corn = {}, {}, {}
+   assert_equal("ABCB", M {apple, banana, corn, banana})
+end
+
+function test_match_repeated_table_var_FAIL_out_of_order()
+   local apple, banana, corn = {}, {}, {}
+   assert_false(M {apple, corn, banana, banana})
+end
+
+function test_destructuring()
+   assert_equal("X is FOO", M { "a", { "b", "FOO" }, "c", "FOO"})
+end
+
+function test_dont_care()
+   assert_equal("third",
+                M { "extract",
+                  { "first", "second", "third", "fourth" }})
+end
+
+function test_match_any()
+   local m = tamale.matcher { { V"_", function(t) return t end } }
+   assert_true(m "any string")
+   assert_true(m(4))
+   assert_true(m {"x", "y", "z"})
+end
+
+
+--Match against three values that add up to 35, and use
+--structural matching to check that the first and third are the same.
+local aba_pt_match = tamale.matcher {
+      { { x=V"X", y=V"Y", z=V"X" },
+        function(t) return t.X + t.Y + t.X end }
+   }
+
+function test_kv_match()
+   assert_equal(35, aba_pt_match {x=15, y=5, z=15 })
+end
+
+function test_kv_match_fail()
+   assert_false(aba_pt_match {x=10, y=20, z=5 })
+end
+
+
+lunatest.run()
